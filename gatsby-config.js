14725f2f25015b05a7da76e87056e378031015ea
adityatheprogrammer <adithya506@gmail.com>
2023-11-01 23:49:13 +0530

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,25 @@ module.exports = {
       options: {
         // The property ID; the tracking code won't be generated without it
         trackingId: "G-JJW7NYLSS8",
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+        // defaults to false
+        enableWebVitalsTracking: true,
       }
     },
     "@chakra-ui/gatsby-plugin",
